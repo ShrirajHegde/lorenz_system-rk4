e0c88f978bce0a9962750b9bdc27880f5c748b7a
Shriraj <shriraj.hegde@gmail.com>
2021-01-13 13:44:54 +0530

    animation script fixed

--- generate_animated_video.sh
+++ generate_animated_video.sh
@@ -3,5 +3,5 @@ gnuplot animate_lorenz_attractor_dat.gnu | ffmpeg -f png_pipe -s:v 800x600 -i pi
 ffmpeg -itsscale 0.83 -i out.mp4 -c:v libx264 -preset slow -crf 22 \
         -profile:v baseline -level 3.0 \
         -movflags +faststart -pix_fmt yuv420p \
-        -c:a libfdk_aac -b:a 128k \ lorenz_attractor.mp4 \
+        -c:a libfdk_aac -b:a 128k "lorenz_attractor.mp4" \
         && rm -f out.mp4
